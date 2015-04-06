f8dd7db2a21f5394318d87bbe6518b3cc5c5fb59
Joe Goett <jgoett154@gmail.com>
2015-04-05 20:24:02 -0400

    Economy now supports the basic item econ now

--- src/main/java/mytown/core/utils/economy/Economy.java
+++ src/main/java/mytown/core/utils/economy/Economy.java
@@ -1,7 +1,9 @@
 package mytown.core.utils.economy;
 
+import mytown.core.utils.PlayerUtils;
 import mytown.core.utils.economy.forgeessentials.ForgeessentialsEconomy;
 import mytown.core.utils.economy.vault.VaultEconomy;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
@@ -13,9 +15,11 @@ import java.util.UUID;
  * @author Joe Goett
  */
 public class Economy {
+    private String costItemName;
     public Class<IEconManager> econManagerClass;
 
     public Economy(String costItemName) {
+        this.costItemName = costItemName;
         if(costItemName.equals("$Vault")) {
             if (MinecraftServer.getServer().getServerModName().contains("cauldron") || MinecraftServer.getServer().getServerModName().contains("mcpc")) {
                 Server server = Bukkit.getServer();
@@ -49,5 +53,38 @@ public class Economy {
         }
 
         return null; // Hopefully this doesn't break things...
+    }
+
+    /**
+     * Takes the amount of money specified.
+     * Returns false if player doesn't have the money necessary
+     */
+    public boolean takeMoneyFromPlayer(EntityPlayer player, int amount) {
+        if(costItemName.startsWith("$")) {
+            IEconManager eco = economyManagerForUUID(player.getUniqueID());
+            if (eco == null) return false;
+            int wallet = eco.getWallet();
+            if (wallet >= amount) {
+                eco.removeFromWallet(amount);
+                return true;
+            }
+            return false;
+        } else {
+            return PlayerUtils.takeItemFromPlayer(player, costItemName, amount);
+        }
+    }
+
+    /**
+     * Takes the amount of money specified.
+     * Returns false if player doesn't have the money necessary
+     */
+    public void giveMoneyToPlayer(EntityPlayer player, int amount) {
+        if (costItemName.startsWith("$")) {
+            IEconManager eco = economyManagerForUUID(player.getUniqueID());
+            if (eco == null) return;
+            eco.addToWallet(amount);
+        } else {
+            PlayerUtils.giveItemToPlayer(player, costItemName, amount);
+        }
     }
 }
