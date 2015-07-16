239b3978a056648dd3024dcef049a452086d8fe4
Joe Goett <jgoett154@gmail.com>
2015-07-16 17:48:49 -0400

    Added OP check to maintenance mode.

--- src/main/java/mytown/core/PlayerTracker.java
+++ src/main/java/mytown/core/PlayerTracker.java
@@ -1,8 +1,10 @@
 package mytown.core;
 
+import mytown.core.utils.PlayerUtils;
 import net.minecraft.entity.player.EntityPlayerMP;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.util.FakePlayer;
 
 public class PlayerTracker {
@@ -10,7 +12,9 @@ public class PlayerTracker {
     @SubscribeEvent
     public void onPlayerLogin(PlayerLoggedInEvent ev) {
         if (Config.maintenanceMode && ev.player instanceof EntityPlayerMP && !(ev.player instanceof FakePlayer)) {
-            ((EntityPlayerMP) ev.player).playerNetServerHandler.kickPlayerFromServer(Config.maintenanceModeMessage);
+            if (!PlayerUtils.isOp(ev.player)) {
+                ((EntityPlayerMP) ev.player).playerNetServerHandler.kickPlayerFromServer(Config.maintenanceModeMessage);
+            }
             return;
         }
     }
