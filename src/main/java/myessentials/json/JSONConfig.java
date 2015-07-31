e7dceee7e5c4f01a8380e352f027b66498fef941
AfterWind <afterwinddesu@gmail.com>
2015-07-31 15:47:38 +0300

    Refactored the JSONConfig.

--- src/main/java/myessentials/json/JSONConfig.java
+++ src/main/java/myessentials/json/JSONConfig.java
@@ -2,9 +2,12 @@ package myessentials.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import myessentials.MyEssentialsCore;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
-import java.io.File;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,12 +18,14 @@ public abstract class JSONConfig<T> {
     /**
      * The path to the file used.
      */
-    protected final String path;
+    protected final String path, name;
     protected final Gson gson;
     protected Type gsonType;
 
-    public JSONConfig(String path) {
+
+    public JSONConfig(String path, String name) {
         this.path = path;
+        this.name = name;
         gson = new GsonBuilder().setPrettyPrinting().create();
     }
 
@@ -34,32 +39,70 @@ public abstract class JSONConfig<T> {
         if(!parent.exists() && !parent.mkdirs()){
             throw new IllegalStateException("Couldn't create dir: " + parent);
         }
-
+        List<T> items = new ArrayList<T>();
         if(!file.exists() || file.isDirectory())
-            create();
+            create(items);
         else {
-            List<T> wrappers = read();
-            update(wrappers);
+            read();
         }
     }
 
     /**
-     * Creates the file if it doesn't exist.
+     * Creates the file if it doesn't exist with the initial given items
      */
-    protected abstract List<T> create();
+    protected void create(List<T> initialItems) {
+        try {
+            Writer writer = new FileWriter(path);
+            gson.toJson(initialItems, gsonType, writer);
+            writer.close();
+            MyEssentialsCore.instance.LOG.info("Created new " + name + " file successfully!");
+        } catch (IOException ex) {
+            MyEssentialsCore.instance.LOG.error(ExceptionUtils.getStackTrace(ex));
+            MyEssentialsCore.instance.LOG.error("Failed to create " + name + " file!");
+        }
+    }
 
     /**
-     * Writes the updated list back to the file.
+     * Writes the given list to the file, completely overwriting it
      */
-    public abstract void write(List<T> items);
+    public void write(List<T> items) {
+        try {
+            Writer writer = new FileWriter(path);
+            gson.toJson(items, gsonType, writer);
+            writer.close();
+            MyEssentialsCore.instance.LOG.info("Updated the " + name + " file successfully!");
+        } catch (IOException ex) {
+            MyEssentialsCore.instance.LOG.error(ExceptionUtils.getStackTrace(ex));
+            MyEssentialsCore.instance.LOG.error("Failed to update " + name + " file!");
+        }
+    }
 
     /**
-     * Reads the file and deletes all invalid values and returns a List with all values being valid.
+     * Reads and returns the validated items.
      */
-    protected abstract List<T> read();
+    protected List<T> read() {
+        List<T> items = new ArrayList<T>();
+
+        try {
+            Reader reader = new FileReader(path);
+            items = gson.fromJson(reader, gsonType);
+            reader.close();
+            MyEssentialsCore.instance.LOG.info("Loaded " + name + " successfully!");
+        } catch (IOException ex) {
+            MyEssentialsCore.instance.LOG.error(ExceptionUtils.getStackTrace(ex));
+            MyEssentialsCore.instance.LOG.error("Failed to read from " + name + " file!");
+        }
+
+        if(!validate(items))
+            write(items);
+
+        return items;
+    }
 
     /**
-     * Updates the file so that it has the previous valid settings plus the missing ones.
+     * Checks for validity and modifies the given list so that is valid.
      */
-    protected abstract void update(List<T> items);
+    protected boolean validate(List<T> items) {
+        return true;
+    }
 }
