a0a6a1a73091c8517672c12fa59ae5a44536f4c7
AfterWind <afterwinddesu@gmail.com>
2015-08-04 21:48:45 +0300

    Added Container class.

---
+++ src/main/java/myessentials/entities/Container.java
@@ -0,0 +1,35 @@
+package myessentials.entities;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Container<T> {
+
+    protected List<T> items = new ArrayList<T>();
+
+    public void add(T item) {
+        items.add(item);
+    }
+
+    public void remove(T item) {
+        items.remove(item);
+    }
+
+    public boolean contains(T item) {
+        return items.contains(item);
+    }
+
+    public List<T> asList() {
+        return ImmutableList.copyOf(items);
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+}
