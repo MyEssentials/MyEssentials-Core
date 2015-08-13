c1ab5a12acace57ec4111b6138b2ce1be48fde99
Joe Goett <jgoett154@gmail.com>
2015-08-12 20:22:38 -0400

    Initial curse api support. (Not tested)

---
+++ src/main/java/myessentials/curse/VersionInfo.java
@@ -0,0 +1,39 @@
+package myessentials.curse;
+
+public class VersionInfo {
+    private int id;
+    private String url;
+    private String name;
+    private String type;
+    private String version;
+    private int downloads;
+    private String created_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public int getDownloads() {
+        return downloads;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+}
