d4408e1bf6a8a6c73280cc80c9698b1b31c3d72e
Joe Goett <jgoett154@gmail.com>
2015-08-12 20:20:46 -0400

    Json config tests. Not working atm, but they are there.

---
+++ src/test/java/myessentials/test/config/TestConfig.java
@@ -0,0 +1,54 @@
+package myessentials.test.config;
+
+import myessentials.new_config.Config;
+
+@Config
+public class TestConfig {
+    @Config.Instance
+    public static TestConfig Data;
+
+    @Config.Group(name = "TypeTest")
+    public static class TypeTestGroup {
+        @Config.Property
+        public static int intTest;
+
+        @Config.Property
+        public static double doubleTest;
+
+        @Config.Property
+        public static float floatTest;
+
+        @Config.Property
+        public static boolean booleanTest;
+
+        @Config.Property
+        public static String stringTest;
+    }
+
+    @Config.Group(name = "ArrayTypeTest")
+    public static class ArrayTypeTestGroup {
+        @Config.Property
+        public static int[] intTest;
+
+        @Config.Property
+        public static double[] doubleTest;
+
+        @Config.Property
+        public static float[] floatTest;
+
+        @Config.Property
+        public static boolean[] booleanTest;
+
+        @Config.Property
+        public static String[] stringTest;
+    }
+
+    @Config.Group(name = "DefaultGroupTest")
+    public static class DefaultTestGroup {
+        @Config.Property(name = "is_a_default")
+        public static String aDefault = "Hello World!";
+
+        @Config.Property(name = "is_overridden")
+        public static String anOverriddenDefault = "Hello World!";
+    }
+}
