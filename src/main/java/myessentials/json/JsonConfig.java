e67028e8aae3aa3af3513f886cebead6b85eb15e
krwminer <krwshop@hotmail.com>
2015-10-18 22:10:44 -0400

    Read after creation of JsonConfig files.

--- src/main/java/myessentials/json/JsonConfig.java
+++ src/main/java/myessentials/json/JsonConfig.java
@@ -45,11 +45,10 @@ public abstract class JsonConfig<T, L extends List<T>> {
         if (!parent.exists() && !parent.mkdirs()) {
             throw new IllegalStateException("Couldn't create dir: " + parent);
         }
-        if (!file.exists() || file.isDirectory())
+        if (!file.exists() || file.isDirectory()) {
             create(items);
-        else {
-            read();
         }
+        read();
     }
 
     /**
