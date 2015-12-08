2b6c75f691374ab80decd5be97e6173e83e41d82
AfterWind <afterwinddesu@gmail.com>
2015-12-09 01:04:04 +0200

    Updated METest

--- src/test/java/myessentials/test/MECTestSuite.java
+++ src/test/java/myessentials/test/MECTestSuite.java
@@ -1,5 +1,7 @@
 package myessentials.test;
 
+import metest.BaseSuite;
+import metest.MinecraftServerStarter;
 import myessentials.test.config.ConfigTest;
 import myessentials.test.datasource.DatasourceTest;
 import myessentials.test.economy.EconomyTest;
@@ -9,10 +11,15 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({EconomyTest.class, DatasourceTest.class, ConfigTest.class})
-public class MECTestSuite {
+public class MECTestSuite extends BaseSuite {
 
     @AfterClass
     public static void tearDown() {
-        throw new RuntimeException("STAHP");
+        System.out.println(MECTestSuite.class.getClassLoader().getClass().getName());
+        try {
+            MinecraftServerStarter.INSTANCE().getGame().getClass().getMethod("stopServer").invoke(MinecraftServerStarter.INSTANCE().getGame());
+        } catch (Exception ex) {
+            throw new RuntimeException();
+        }
     }
 }
