badbfcea6cd0b8f3ec54287d8e3e0dae41e35fd5
AfterWind <afterwinddesu@gmail.com>
2015-12-09 19:00:59 +0200

    Moved the tearDown method up a level.

--- src/test/java/myessentials/test/MECTestSuite.java
+++ src/test/java/myessentials/test/MECTestSuite.java
@@ -1,11 +1,9 @@
 package myessentials.test;
 
 import metest.BaseSuite;
-import metest.MinecraftServerStarter;
 import myessentials.test.config.ConfigTest;
 import myessentials.test.datasource.DatasourceTest;
 import myessentials.test.economy.EconomyTest;
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -13,13 +11,4 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({EconomyTest.class, DatasourceTest.class, ConfigTest.class})
 public class MECTestSuite extends BaseSuite {
 
-    @AfterClass
-    public static void tearDown() {
-        System.out.println(MECTestSuite.class.getClassLoader().getClass().getName());
-        try {
-            MinecraftServerStarter.INSTANCE().getGame().getClass().getMethod("stopServer").invoke(MinecraftServerStarter.INSTANCE().getGame());
-        } catch (Exception ex) {
-            throw new RuntimeException();
-        }
-    }
 }
