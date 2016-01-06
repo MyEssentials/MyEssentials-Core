a996ac7211f444ca14252d6e2d952648232910ad
AfterWind <afterwinddesu@gmail.com>
2016-01-06 11:24:26 +0200

    Added tests for most existing entities.

---
+++ src/test/java/myessentials/test/ChunkPosTest.java
@@ -0,0 +1,20 @@
+package myessentials.test;
+
+import junit.framework.Assert;
+import myessentials.entities.ChunkPos;
+import org.junit.Test;
+
+public class ChunkPosTest extends MECTest {
+
+    @Test
+    public void chunkPosEquality() {
+        ChunkPos cp1 = new ChunkPos(1, 1, 0);
+        ChunkPos cp2 = new ChunkPos(1, 1, 0);
+        ChunkPos cp3 = new ChunkPos(1, 1, 1);
+        ChunkPos cp4 = new ChunkPos(2, 1, 0);
+
+        Assert.assertTrue("Two ChunkPos objects with same coords are not equal", cp1.equals(cp2));
+        Assert.assertFalse("Two ChunkPos objects with different dimension id are equal", cp1.equals(cp3));
+        Assert.assertFalse("Two ChunkPos objects with different coords are equal", cp1.equals(cp4));
+    }
+}
--- src/test/java/myessentials/test/MECTestSuite.java
+++ src/test/java/myessentials/test/MECTestSuite.java
@@ -5,7 +5,12 @@ import myessentials.test.config.ConfigTest;
 import myessentials.test.datasource.DatasourceTest;
 import myessentials.test.economy.EconomyForgeEssentialsTest;
 import myessentials.test.economy.EconomyItemTest;
+import myessentials.test.entities.BlockPosTest;
+import myessentials.test.entities.EntityPosTest;
+import myessentials.test.entities.TreeTest;
+import myessentials.test.entities.VolumeTest;
 import myessentials.test.entities.sign.SignTest;
+import myessentials.test.entities.tool.ToolTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -16,7 +21,14 @@ import org.junit.runners.Suite;
         EconomyForgeEssentialsTest.class,
         DatasourceTest.class,
         ConfigTest.class,
-        SignTest.class
+        SignTest.class,
+        ToolTest.class,
+        BlockPosTest.class,
+        ChunkPosTest.class,
+        EntityPosTest.class,
+        TreeTest.class,
+        VolumeTest.class
+
 })
 
 public class MECTestSuite extends BaseSuite {
