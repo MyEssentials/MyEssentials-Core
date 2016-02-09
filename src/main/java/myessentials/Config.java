81d38d439b3f0f89ec2b6d38b8ac459a8ee78bb1
AfterWind <afterwinddesu@gmail.com>
2016-02-09 17:17:00 +0200

    Added Localization and formatting for Volume.

--- src/main/java/myessentials/Config.java
+++ src/main/java/myessentials/Config.java
@@ -16,4 +16,9 @@ public class Config extends ConfigTemplate {
             "maintenanceModeMessage", "general",
             "Custom message to display when in maintenance mode",
             "Server is in maintenance mode currently. Please come back later.");
+
+    public ConfigProperty<String> localization = new ConfigProperty<String>(
+            "localization", "general",
+            "Localization used for the mod",
+            "en_US");
 }
