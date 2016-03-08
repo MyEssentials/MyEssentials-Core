e4c1ec9b9f7c501a50e887725daa52efe0a4ea25
CJ Miller <cjmiller721@gmail.com>
2016-03-08 18:56:52 -0500

    Cleaned Imports.
    Removed unused imports.

--- src/main/java/myessentials/utils/ClassUtils.java
+++ src/main/java/myessentials/utils/ClassUtils.java
@@ -2,11 +2,9 @@ package myessentials.utils;
 
 import net.minecraft.server.MinecraftServer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
