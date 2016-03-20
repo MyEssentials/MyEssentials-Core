1a5ebdd3f30840169f6ef0c79630ca58a61f1012
AfterWind <afterwinddesu@gmail.com>
2016-03-20 17:46:10 +0200

    Made the Teleport class usable again

--- src/main/java/myessentials/teleport/Teleport.java
+++ src/main/java/myessentials/teleport/Teleport.java
@@ -1,83 +1,22 @@
 package myessentials.teleport;
 
-import net.minecraft.entity.player.EntityPlayer;
+import myessentials.entities.api.EntityPos;
+import myessentials.utils.PlayerUtils;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.common.DimensionManager;
 
 /**
  * Retains the needed information for the action of teleporting a player to a certain position.
  */
-public class Teleport {
-    private int dim;
-    private float x, y, z, yaw, pitch;
+public class Teleport extends EntityPos {
 
-    public Teleport(int dim, float x, float y, float z, float yaw, float pitch) {
-        setDim(dim);
-        setPosition(x, y, z);
-        setRotation(yaw, pitch);
+    public Teleport(double x, double y, double z, int dim) {
+        super(x, y, z, dim);
     }
 
-    public Teleport(int dim, float x, float y, float z) {
-        this(dim, x, y, z, 0, 0);
-    }
-
-    //Used when a player is riding an entity. eg pig, horse
-    public void teleport(EntityPlayer pl, boolean canRide){
-        if (pl.dimension != dim) {
-            MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension((EntityPlayerMP)pl, dim, new EssentialsTeleporter(MinecraftServer.getServer().worldServerForDimension(dim)));
-        }
-        if(pl.isRiding() && pl.ridingEntity != null  && pl.ridingEntity.isEntityAlive() && canRide) {
-            pl.ridingEntity.setPosition(x, y, z);
-            pl.ridingEntity.setPositionAndRotation(x, y, z, yaw, pitch);
-        }
-        pl.setPositionAndUpdate(x, y, z);
-        pl.setPositionAndRotation(x, y, z, yaw, pitch);
-    }
-
-    public void teleport(EntityPlayer pl) {
-        teleport(pl, false);
-    }
-
-    public Teleport setDim(int dim) {
-        this.dim = dim;
-        return this;
-    }
-
-    public Teleport setPosition(float x, float y, float z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        return this;
-    }
-
-    public Teleport setRotation(float yaw, float pitch) {
-        this.yaw = yaw;
-        this.pitch = pitch;
-        return this;
-    }
-
-    public int getDim() {
-        return dim;
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public float getZ() {
-        return z;
-    }
-
-    public float getYaw() {
-        return yaw;
-    }
-
-    public float getPitch() {
-        return pitch;
+    /**
+     * Executes the teleportation of the given player
+     */
+    public void execute(EntityPlayerMP player) {
+        PlayerUtils.teleport(player, getDim(), getX(), getY(), getZ());
     }
 }
